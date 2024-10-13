fc6934ddd800730924b7ff950593414e39f23e9e
David Volovskiy <devolov@gmail.com>
2024-10-13 19:50:17 -0400

    Modified worldclock2 to use utc abrev logic

--- movement/watch_faces/clock/world_clock2_face.c
+++ movement/watch_faces/clock/world_clock2_face.c
@@ -61,22 +61,6 @@ static inline uint8_t find_selected_zone(world_clock2_state_t *state, int direct
     return i;
 }
 
-static char *get_zone_abrev(uint8_t zone) {
-    int nullCount = 0;
-    int i = 0;
-    int zone_abrevs_len = sizeof(zone_abrevs) / sizeof(zone_abrevs[0]);
-    if (zone != 0) {
-        while (zone_abrevs[i] != '\0' || nullCount < zone - 1) {
-            if (zone_abrevs[i] == '\0')
-                nullCount++;
-            i++;
-        }
-        i++;
-    }
-    if (i >= zone_abrevs_len) return "  ";
-    return (char *)(zone_abrevs + i);
-}
-
 /* Beep when zone is enabled. An octave up */
 static void beep_enable() {
     watch_buzzer_play_note(BUZZER_NOTE_G7, 50);
@@ -103,7 +87,6 @@ void world_clock2_face_setup(movement_settings_t *settings, uint8_t watch_face_i
         /* Start in settings mode */
         world_clock2_state_t *state = (world_clock2_state_t *) * context_ptr;
         state->current_mode = WORLD_CLOCK2_MODE_SETTINGS;
-        snprintf(state->zone_abrev, sizeof(state->zone_abrev), "%.2s", get_zone_abrev(state->current_zone));
     }
 }
 
@@ -184,13 +167,13 @@ static bool mode_display(movement_event_t event, movement_settings_t *settings, 
 			watch_start_tick_animation(500);
 
 		    sprintf(buf, "%.2s%2d%2d%02d  ",
-                            state->zone_abrev,
+                            (char *)(zone_abrevs + zone_defns[state->current_zone].abrev_formatter),
                             date_time.unit.day,
                             date_time.unit.hour,
                             date_time.unit.minute);
 		} else {
 		    sprintf(buf, "%.2s%2d%2d%02d%02d",
-			                state->zone_abrev,
+			                (char *)(zone_abrevs + zone_defns[state->current_zone].abrev_formatter),
                             date_time.unit.day,
                             date_time.unit.hour,
                             date_time.unit.minute,
@@ -202,7 +185,6 @@ static bool mode_display(movement_event_t event, movement_settings_t *settings, 
 	case EVENT_ALARM_BUTTON_UP:
 	    state->current_zone = find_selected_zone(state, FORWARD);
         state->previous_date_time = REFRESH_TIME;
-        snprintf(state->zone_abrev, sizeof(state->zone_abrev), "%.2s", get_zone_abrev(state->current_zone));
 	    break;
 	case EVENT_LIGHT_BUTTON_DOWN:
 	    /* Do nothing. */
@@ -210,7 +192,6 @@ static bool mode_display(movement_event_t event, movement_settings_t *settings, 
 	case EVENT_LIGHT_BUTTON_UP:
 	    state->current_zone = find_selected_zone(state, BACKWARD);
         state->previous_date_time = REFRESH_TIME;
-        snprintf(state->zone_abrev, sizeof(state->zone_abrev), "%.2s", get_zone_abrev(state->current_zone));
 	    break;
 	case EVENT_LIGHT_LONG_PRESS:
 	    movement_illuminate_led();
@@ -264,7 +245,7 @@ static bool mode_settings(movement_event_t event, movement_settings_t *settings,
 	     * corresponding compiler warnings.
 	     */
 	    sprintf(buf, "%.2s%2d %c%02d%02d",
-                    state->zone_abrev,
+                    (char *)(zone_abrevs + zone_defns[state->current_zone].abrev_formatter),
                     state->current_zone % 100,
                     hours < 0 ? '-' : '+',
                     abs(hours) % 24,
@@ -283,11 +264,9 @@ static bool mode_settings(movement_event_t event, movement_settings_t *settings,
 	    break;
 	case EVENT_ALARM_BUTTON_UP:
 	    state->current_zone = mod(state->current_zone + FORWARD, NUM_ZONE_NAMES);
-        snprintf(state->zone_abrev, sizeof(state->zone_abrev), "%.2s", get_zone_abrev(state->current_zone));
 	    break;
 	case EVENT_LIGHT_BUTTON_UP:
 	    state->current_zone = mod(state->current_zone + BACKWARD, NUM_ZONE_NAMES);
-        snprintf(state->zone_abrev, sizeof(state->zone_abrev), "%.2s", get_zone_abrev(state->current_zone));
 	    break;
 	case EVENT_LIGHT_BUTTON_DOWN:
 	    /* Do nothing */
