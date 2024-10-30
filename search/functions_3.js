27e532433b5c687e9ffb4aa856710f111ef0df96
voloved <voloved@users.noreply.github.com>
2024-10-30 23:35:40 +0000

    deploy: b69cd117f96cdf6a46a4801c7fcf95e0d6ab5047

---
+++ search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['days_5fin_5fmonth_0',['days_in_month',['../group__utility.html#gad3a725af3abd77d08d503f665f2ccd70',1,'days_in_month(uint8_t month, uint16_t year):&#160;watch_utility.c'],['../group__utility.html#gad3a725af3abd77d08d503f665f2ccd70',1,'days_in_month(uint8_t month, uint16_t year):&#160;watch_utility.c']]]
+];
